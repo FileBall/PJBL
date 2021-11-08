bea51746c62833fdceabe2a419b78974343e44f1
FileBall <90704867+FileBall@users.noreply.github.com>
2021-11-08 15:26:36 -0300

    Create scriptTelaSuporte.js

---
+++ js/scriptTelaSuporte.js
@@ -0,0 +1,4 @@
+function apagarTextoProblema(){
+    document.getElementById("textoProblema").value = "";
+    alert("Sua mensagem foi enviada, ela será analisada e depois respondida diretamente para o E-mail cadastrado.")
+}
