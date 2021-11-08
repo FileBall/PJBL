555021d3ff56a02c68ea60646fbe7c5ddc0a85df
FileBall <90704867+FileBall@users.noreply.github.com>
2021-11-08 15:31:29 -0300

    Create scriptTelaAvaliacao.js

---
+++ js/scriptTelaAvaliacao.js
@@ -0,0 +1,14 @@
+function apagarElementos(){
+
+    document.getElementById("respostaAvaliacaoA").value = "";
+    document.getElementById("respostaAvaliacaoB").value = "";
+    document.getElementById("respostaAvaliacaoC").value = "";
+    document.getElementById("respostaAvaliacaoD").value = "";
+    document.getElementById("respostaAvaliacaoE").value = "";
+    document.getElementById("respostaAvaliacaoF").value = "";
+    document.getElementById("respostaAvaliacaoG").value = "";
+    document.getElementById("respostaAvaliacaoH").value = "";
+    document.getElementById("respostaAvaliacaoI").value = "";
+    alert("Sua avaliação foi enviada! Você já pode voltar.")
+
+}
