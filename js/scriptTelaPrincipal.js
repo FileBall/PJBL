4af9618f4f8e326f5974bdd3591040da0fcc4cfa
FileBall <90704867+FileBall@users.noreply.github.com>
2021-11-08 15:21:03 -0300

    Create scriptTelaPrincipal.js

---
+++ js/scriptTelaPrincipal.js
@@ -0,0 +1,21 @@
+function ApagarTexto(){
+    document.getElementById("caixaTexto").value = "";
+    alert ("Sua mensagem foi enviada!");
+
+function abrirMario(){
+
+    document.getElementById("chatMario").style.display = "block";
+}
+function fecharMario(){
+
+    document.getElementById("chatMario").style.display = "none";
+}
+function abrirLeticia(){
+
+    document.getElementById("chatLeticia").style.display = "block";
+}
+function fecharLeticia(){
+
+    document.getElementById("chatLeticia").style.display = "none";
+}
+function apagarElementos(){
