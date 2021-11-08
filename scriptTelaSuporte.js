bf123a5b09e9b252c3fa2ac30af9ce88ab700e2f
FileBall <90704867+FileBall@users.noreply.github.com>
2021-11-08 15:27:13 -0300

    Delete scriptTelaSuporte.js

--- scriptTelaSuporte.js
+++
@@ -1,4 +0,0 @@
-function apagarTextoProblema(){
-    document.getElementById("textoProblema").value = "";
-    alert("Sua mensagem foi enviada, ela será analisada e depois respondida diretamente para o E-mail cadastrado.")
-}
